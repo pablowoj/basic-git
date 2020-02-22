2330a6a655332ac37683c6c4f1dbf5b0bde12f94
pablowoj <61342976+pablowoj@users.noreply.github.com>
2020-02-22 11:34:43 +0100

    super kod

--- src/akademia/Main.java
+++ src/akademia/Main.java
@@ -8,5 +8,8 @@ public class Main {
         System.out.println("Test3");
 
         System.out.println("tu zaczyna sie versja 1.0.0");
+
+        System.out.println("tutaj lezy super kod w wersji 1.0.1");
+
     }
 }
